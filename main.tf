7175bfe8e8cbcb0e864c28340b9f428ad3beafa8
teja <teja.awsclouds@gmial.com>
2022-10-15 14:48:22 +0530

    some changes

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 resource "aws_s3_bucket" "kind_heart_me" {
 bucket = "hdppsa4"
-
 }
 
 resource "aws_vpc" "my_vpc" {
-    
     cidr_block = "192.168.0.0/16"
     tags       = {
     name       = "mynetwork"    
     }
-  
-}
+ }
